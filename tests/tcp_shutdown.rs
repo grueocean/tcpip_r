7e80735ad961f9fe769806a65b6a56259c4b6e03
grueocean <38178914+grueocean@users.noreply.github.com>
2025-02-23 14:56:17 +0900

    test: add test_normal_shutdown_client_drop

--- tests/tcp_shutdown.rs
+++ tests/tcp_shutdown.rs
@@ -1,7 +1,8 @@
 mod common;
 use anyhow::{Context, Result};
 use common::{
-    check_stdout_pattern, child_wait_with_timeout, cleanup_env, dump_stderr, dump_stdout, setup_env,
+    check_stdout_pattern, child_wait_with_timeout, cleanup_env, dump_stderr, dump_stdout,
+    insert_drop, setup_env,
 };
 use serial_test::serial;
 use std::ops::Add;
@@ -24,6 +25,7 @@ const TESTAPP_TCP_CLOSE_CLIENT: &str = "_test_tcp_close_client";
 const TESTAPP_TCP_CLOSE_SERVER: &str = "_test_tcp_close_server";
 const TEST_INITIALIZE: u64 = 50; // msec
 const TEST_TIMEOUT: u64 = 10; // sec
+const TEST_TIMEOUT_DROP: u64 = 30; // sec
 const CLIENT_CONNECTTED: &str = "Socket connected!";
 const SERVER_ACCEPTED: &str = "Socket accepted!";
 const SHUTDOWN: &str = "Socket shutdown.";
@@ -156,6 +158,74 @@ fn test_normal_shutdown_both() -> Result<()> {
                 && check_stdout_pattern(&mut server, &expected_server_stdout)?
                 && client_status.success()
                 && check_stdout_pattern(&mut client, &expected_client_stdout)?
+            {
+                suc = true;
+            }
+        }
+    }
+    server.kill()?;
+    server.wait()?;
+    client.kill()?;
+    client.wait()?;
+    dump_stderr(&mut server)?;
+    dump_stderr(&mut client)?;
+    cleanup_env(env_num)?;
+    assert!(suc, "Connection is not closed correctly.");
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_normal_shutdown_client_drop() -> Result<()> {
+    let mut suc = false;
+    let expected_client_stdout = [CLIENT_CONNECTTED, SHUTDOWN];
+    let env_num: usize = 1;
+    setup_env(env_num)?;
+    insert_drop(env_num, 30)?;
+    thread::sleep(Duration::from_millis(TEST_INITIALIZE));
+    let mut server = Command::new("sudo")
+        .arg("ip")
+        .arg("netns")
+        .arg("exec")
+        .arg("Tcpip0")
+        .arg("nc")
+        .arg("-l")
+        .arg(TCP_SERVER_PORT.to_string())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .context("Failed to execut server (nc).")?;
+    let mut client = Command::new("sudo")
+        .arg("ip")
+        .arg("netns")
+        .arg("exec")
+        .arg("Dev0")
+        .arg(format!("{}{}", TESTAPP_PATH, TESTAPP_TCP_CLOSE_CLIENT))
+        .arg("--iface")
+        .arg("d0")
+        .arg("--network")
+        .arg(format!("{}/{}", NETWORK_DEV0, SUBNETMASK))
+        .arg("--gateway")
+        .arg(GATEWAY.to_string())
+        .arg("--dst")
+        .arg(NETWORK_TCPIP0.to_string())
+        .arg("--port")
+        .arg(TCP_SERVER_PORT.to_string())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .context(format!("Failed to execute {}.", TESTAPP_TCP_CLOSE_CLIENT))?;
+    if let Some(client_status) =
+        child_wait_with_timeout(&mut client, Duration::from_secs(TEST_TIMEOUT_DROP))?
+    {
+        println!("client status: {:?}", client_status);
+        if let Some(server_status) =
+            child_wait_with_timeout(&mut server, Duration::from_secs(TEST_TIMEOUT))?
+        {
+            println!("server status: {:?}", server_status);
+            if client_status.success()
+                && check_stdout_pattern(&mut client, &expected_client_stdout)?
+                && server_status.success()
             {
                 suc = true;
             }
